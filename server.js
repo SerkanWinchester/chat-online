ee102d149b341a1af05b53377651841309afbb53
SerkanWinchester <serkanwinchester@outlook.com.br>
2025-08-20 19:45:45 -0300

    server.js

--- server.js
+++ server.js
@@ -10,13 +10,22 @@ const io = socketIo(server);
 // Define a porta do servidor
 const PORT = process.env.PORT || 3000;
 
-// Serve os arquivos estáticos da pasta onde está o index.html
+// Armazena a lista de usuários online
+const users = {};
+
+// Serve os arquivos estáticos da pasta raiz
 app.use(express.static(path.join(__dirname)));
 
-// Quando um cliente se conectar, ele vai receber uma mensagem
+// Quando um cliente se conectar
 io.on('connection', (socket) => {
   console.log('Novo usuário conectado');
   
+  // O cliente envia seu nome e cor para ser adicionado à lista
+  socket.on('user connected', (user) => {
+    users[socket.id] = user;
+    io.emit('user list update', Object.values(users));
+  });
+
   // Quando o servidor recebe uma mensagem do chat, ele retransmite para todos
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
@@ -25,6 +34,8 @@ io.on('connection', (socket) => {
   // Quando um cliente desconecta
   socket.on('disconnect', () => {
     console.log('Usuário desconectado');
+    delete users[socket.id];
+    io.emit('user list update', Object.values(users));
   });
 });
 
