b682d49c19f96544d4db4c8d0ef8c656baacb963
SerkanWinchester <serkanwinchester@outlook.com.br>
2025-08-22 20:41:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,32 +8,61 @@ const io = socketIo(server);
 
 const PORT = process.env.PORT || 3000;
 
-// Serve os arquivos da pasta 'public'
 app.use(express.static('public'));
 
-let users = {};
+let users = {}; // Armazena { "socket.id": "username" }
 
-// Lógica do chat em tempo real
 io.on('connection', (socket) => {
-    // Quando um usuário entra
-    socket.on('join', (username) => {
+    // Quando um usuário faz login
+    socket.on('user_login', (username) => {
         socket.username = username;
-        users[username] = socket.id;
-        io.emit('updateUsers', Object.keys(users));
-        socket.broadcast.emit('message', { user: 'Sistema', text: `${username} entrou no chat.` });
+        users[socket.id] = username;
+        // Envia a lista atualizada de usuários para todos
+        io.emit('update_user_list', Object.values(users));
+        // Avisa a todos que um novo usuário entrou
+        socket.broadcast.emit('system_message', `${username} entrou no chat.`);
     });
 
-    // Quando uma mensagem é enviada
-    socket.on('sendMessage', (data) => {
-        io.emit('message', { user: data.user, text: data.text });
+    // Quando uma mensagem geral é enviada
+    socket.on('send_general_message', (data) => {
+        // Envia a mensagem para todos, incluindo o remetente
+        io.emit('receive_general_message', data);
+    });
+
+    // Quando uma mensagem privada é enviada
+    socket.on('send_private_message', (data) => {
+        const recipientSocketId = Object.keys(users).find(key => users[key] === data.to);
+        if (recipientSocketId) {
+            // Envia para o destinatário
+            io.to(recipientSocketId).emit('receive_private_message', data);
+        }
+        // Envia de volta para o remetente
+        socket.emit('receive_private_message', data);
+    });
+
+    // Quando um puxão é enviado
+    socket.on('send_nudge', (data) => {
+        const recipientSocketId = Object.keys(users).find(key => users[key] === data.to);
+        if (recipientSocketId) {
+            io.to(recipientSocketId).emit('nudge_received', { from: data.from });
+        }
+    });
+
+    // Quando um usuário está digitando
+    socket.on('user_typing', (username) => {
+        socket.broadcast.emit('show_typing_indicator', username);
+    });
+    socket.on('user_stopped_typing', () => {
+        socket.broadcast.emit('hide_typing_indicator');
     });
 
     // Quando um usuário se desconecta
     socket.on('disconnect', () => {
-        if (socket.username) {
-            delete users[socket.username];
-            io.emit('updateUsers', Object.keys(users));
-            io.emit('message', { user: 'Sistema', text: `${socket.username} saiu do chat.` });
+        const username = users[socket.id];
+        if (username) {
+            delete users[socket.id];
+            io.emit('update_user_list', Object.values(users));
+            io.emit('system_message', `${username} saiu do chat.`);
         }
     });
 });
